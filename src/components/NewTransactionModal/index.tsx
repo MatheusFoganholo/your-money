5c3869dabf4953edc3ae0ce406b01dd4c933cfe9
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-03-21 16:51:21 -0300

    Adding react modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,52 @@
+import Modal from 'react-modal';
+import Income from '../../assets/income.svg';
+import Outcome from '../../assets/outcome.svg';
+import CloseModalButton from '../../assets/close.svg';
+import { Container, TransactionTypeContainer } from './styles';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={CloseModalButton} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input placeholder="Título" />
+        <input placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={Income} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={Outcome} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
